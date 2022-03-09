ab127045318c292bd76321587eb3e2409e78d368
Bojan Kelecevic <b.kelecevic97@gmail.com>
2022-03-09 13:24:16 +0100

    Rjesen treci zadatak

--- src/Vjezba.java
+++ src/Vjezba.java
@@ -3,11 +3,12 @@ public class Vjezba {
     public static void main(String[] args) {
         patern1(5);
         patern2(5);
+        patern3(5);
     }
 
     static void patern1(int brojRedova){
         /*
-        Zadatak: 5  redova
+        1.Zadatak: 5  redova
 
          *
          **
@@ -32,7 +33,7 @@ public class Vjezba {
 
     static void patern2(int brojRedova){
        /*
-        Zadatak: 5  redova
+        2.Zadatak: 5  redova
 
          *****
          ****
@@ -52,5 +53,29 @@ public class Vjezba {
             System.out.println();
         }
 
+    }
+
+    static void patern3(int brojRedova){
+        /*
+        3.Zadatak: 5  redova
+
+         1
+         1 2
+         1 2 3
+         1 2 3 4
+         1 2 3 4 5
+
+         */
+
+        //Prolazak kroz redove
+        for(int red = 1; red<=brojRedova; red++){
+
+            //Prolazak kroz kolone, broj kolona je jednak broju reda, npr. dugi red ---> dve kolone
+            for(int kolona = 1; kolona<=red; kolona++){
+                System.out.print(kolona+" ");   //Printanje brojeva od 1 do broja kolona
+            }
+            //Prelazak u sledecu liniju za printanje iduceg reda
+            System.out.println();
+        }
     }
 }
